7f0f23f4b5f817a9b5216d4c10cc4d57fbcd0bbb
jucavi <jcarvi@gmail.com>
2024-04-29 15:36:15 +0200

    add rename physical folders with uuid as a name

--- src/main/java/com/example/springmigrate/service/IFileTypeLogicalService.java
+++ src/main/java/com/example/springmigrate/service/IFileTypeLogicalService.java
@@ -1,11 +1,10 @@
 package com.example.springmigrate.service;
 
-import com.example.springmigrate.dto.FileTypeNodeDto;
 
 import java.io.IOException;
 import java.util.Map;
 
 public interface IFileTypeLogicalService {
 
-    public Map<String, String> findAllFileTypes() throws IOException;
+    Map<String, String> findAllFileTypes() throws IOException;
 }
